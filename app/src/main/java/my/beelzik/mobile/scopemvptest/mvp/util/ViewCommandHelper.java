f4a82365e5981903795b57e4aee87a6f22a68334
andrey.agashkov <andrey.agashkov@voodoo-mobile.com>
2016-11-07 02:11:24 +0700

    немного допилил ViewCommandHelper и CommandSubject

--- app/src/main/java/my/beelzik/mobile/scopemvptest/mvp/util/ViewCommandHelper.java
+++ app/src/main/java/my/beelzik/mobile/scopemvptest/mvp/util/ViewCommandHelper.java
@@ -1,8 +1,6 @@
 package my.beelzik.mobile.scopemvptest.mvp.util;
 
-import android.util.Log;
-
-import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
 
 /**
  * Created by Andrey on 05.11.2016.
@@ -12,51 +10,44 @@ public class ViewCommandHelper<V> {
 
     private static final int MAX_COMMAND_IN_ROW = 16;
 
-    private CommandSubject<ViewCommand<V>> mReplaySubject;
-    private Subscription mSubscription;
-
-    //V mView;
+    private CommandSubject<ViewCommand<V>> mCommandSubject;
+    private CompositeSubscription mSubscription;
 
     public ViewCommandHelper() {
         createSubject();
     }
 
     private void createSubject() {
-        mReplaySubject = CommandSubject.createWithSize(MAX_COMMAND_IN_ROW);
+        mCommandSubject = CommandSubject.createWithSize(MAX_COMMAND_IN_ROW);
     }
 
     public void sendCommand(ViewCommand<V> command) {
-        mReplaySubject.onNext(command);
+        mCommandSubject.onNext(command);
     }
 
     public void unsubscribe() {
-        //mView = null;
-        mSubscription.unsubscribe();
+        if (mSubscription != null) {
+            mSubscription.unsubscribe();
+            mSubscription = null;
+        }
     }
 
     private static final String TAG = "ViewCommandHelper";
 
     public void subscribe(V view) {
-        //mView = view;
-        mSubscription = mReplaySubject.subscribe(command -> {
-                    Log.d(TAG, "command type: " + command.getClass());
-                    Log.d(TAG, "command instanceof ViewCommandSingle: " + (command instanceof ViewCommandSingle));
+        if (mSubscription == null) {
+            mSubscription = new CompositeSubscription();
+        }
+        mSubscription.add(mCommandSubject.subscribe(command -> {
                     command.emmit(view);
                     if (command instanceof ViewCommandSingle) {
-                        mReplaySubject.remove(command);
+                        mCommandSubject.remove(command);
                     }
                 }
-        );
+        ));
     }
 
     public void cleanCommands() {
-        mReplaySubject.clear();
-        /*mReplaySubject.onCompleted();
-        mSubscription.unsubscribe();
-        createSubject();*/
-        /*if (mView != null) {
-            subscribe(mView);
-        }*/
-
+        mCommandSubject.clear();
     }
 }
